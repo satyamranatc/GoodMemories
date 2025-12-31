5328d61fdb99965c4ac5d23e8f6095c911e6dea2
Satyam Rana <satyamrana@Satyams-MacBook-Air.local>
2025-12-31 13:54:54 +0530

    Cors Fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://goodmemories-frontend.onrender.com"],
   credentials: true
 }));
 app.use(express.json());
